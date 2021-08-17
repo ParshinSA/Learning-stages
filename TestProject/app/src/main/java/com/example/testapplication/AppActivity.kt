d71c1c265157efc15f40a97224b66958bb999666
ParshinSA <Parshin.Sergey222@mail.ru>
2021-08-17 21:02:57 +0300

    Second test commit

--- TestProject/app/src/main/java/com/example/testapplication/AppActivity.kt
+++ TestProject/app/src/main/java/com/example/testapplication/AppActivity.kt
@@ -3,9 +3,13 @@ package com.example.testapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * Commentary
+         * */
     }
 }
