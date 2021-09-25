660d9265a1a52193d91902a20d37b34400c82996
ParshinSA <Parshin.Sergey222@mail.ru>
2021-09-25 12:21:22 +0300

    Created new project LearningGIT

---
+++ LearninGIT/app/src/main/java/com/example/learnin_git/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.learnin_git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    /**
+     * Тестовый проект, создан для практики работы с втками GIT
+     *
+     * Проект создан на ветке main,
+     * произведу add, commit, push
+     *
+     * создам новую ветвь, в нее внесу изменения
+     * (>>> Вношу изменения из ветки Learning-GIT <<<) todo:изменения
+     * произведу add, commit, push, merge
+     *
+     * вернусь на ветку main
+     * */
+}
