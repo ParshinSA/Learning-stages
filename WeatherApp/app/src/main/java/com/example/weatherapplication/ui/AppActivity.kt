71c2b5c742ee8a029133d757230c8ba27a68ad47
ParshinSA <Parshin.Sergey222@mail.ru>
2021-11-11 11:30:57 +0300

    rename fragmet_list_of_city.xml -> fragment_list_of_city.xml (n)

--- WeatherApp/app/src/main/java/com/example/weatherapplication/ui/AppActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapplication/ui/AppActivity.kt
@@ -2,19 +2,8 @@ package com.example.weatherapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
-import androidx.activity.viewModels
-import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.DialogFragment
 import com.example.weatherapplication.R
-import com.example.weatherapplication.data.datetime.DateTimeFormat
-import com.example.weatherapplication.data.datetime.DateTimeViewModel
-import com.example.weatherapplication.databinding.ActivityAppBinding
-import kotlinx.coroutines.delay
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- WeatherApp/app/src/main/java/com/example/weatherapplication/ui/listofcity/ListOfCityFragment.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapplication/ui/listofcity/ListOfCityFragment.kt
@@ -2,15 +2,27 @@ package com.example.weatherapplication.ui.listofcity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.weatherapplication.R
+import com.example.weatherapplication.databinding.FragmentListOfCityBinding
 
-class ListOfCityFragment : Fragment(R.layout.fragmet_list_of_city) {
+class ListOfCityFragment : Fragment(R.layout.fragment_list_of_city) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _bind: FragmentListOfCityBinding? = null
+    private val bind: FragmentListOfCityBinding
+        get() = _bind!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         Log.d("SystemLogging", "ListOfCityFragment / onCreate")
-
+        _bind = FragmentListOfCityBinding.inflate(layoutInflater, container, false)
+        return bind.root
     }
 
 }
